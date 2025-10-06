6dd5f010dc794b0e6ae8df52bb05f57cd28e3336
Shivam Kumar Pandey <itsshivampandey@gmail.com>
2025-10-06 15:32:27 +0530

    Added early return for no items

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -131,18 +131,30 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 function Footer({ items }) {
+  // Early return if no items
+  if (!items.length) {
+    return (
+      <footer className="stats">
+        <p>
+          <em>Add some items to your packing list 🧳</em>
+        </p>
+      </footer>
+    );
+  }
   let allItems = items.length;
   const packedItems = items.filter((item) => item.packed).length;
   const percentage = Math.round((packedItems / allItems) * 100);
 
   return (
     <footer className="stats">
-      <em>
-        {percentage === 100 && allItems > 0
-          ? "You are ready to go ✈️"
-          : `You have ${allItems} items on you list, and you already packed
-        ${" " + packedItems} (${Number.isNaN(percentage) ? "0" : percentage}%)`}
-      </em>
+      <p>
+        <em>
+          {percentage === 100 && allItems > 0
+            ? "You are ready to go ✈️"
+            : `You have ${allItems} items on you list, and you already packed
+        ${" " + packedItems} (${percentage}%)`}
+        </em>
+      </p>
     </footer>
   );
 }
