a7d482d35acfb025bccc97adc7a1ebda056c74bb
Shivam Kumar Pandey <itsshivampandey@gmail.com>
2025-10-08 13:26:33 +0530

    Calcualtor is working

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -1,9 +1,86 @@
+import { useState } from "react";
+
 function App() {
+  const [bill, setBill] = useState();
+  const [tipAmount, setTipAmount] = useState(0);
+  const [friendTip, setFriendTip] = useState(0);
+
+  function reset() {
+    setBill(0);
+    setTipAmount(0);
+    setFriendTip(0);
+  }
+
+  const totalTip =
+    Number((bill / 100) * tipAmount) + Number((bill / 100) * friendTip);
+
+  const totalBill = Number(bill) + totalTip;
+
   return (
     <div className="App">
-      <h1>Tip Calculator</h1>
+      <form>
+        <label for="billAmount">How much was the bill?</label>
+        <input
+          type="number"
+          id="billAmount"
+          name="billAmount"
+          value={bill}
+          onChange={(e) => setBill(e.target.value)}
+        />
+        <Tip
+          labelFor="tipAmount"
+          label="How much would you like to tip?"
+          tipAmount={tipAmount}
+          setTipAmount={setTipAmount}
+        >
+          <option value="0">0%</option>
+          <option value="10">10%</option>
+          <option value="15">15%</option>
+          <option value="20">20%</option>
+        </Tip>
+
+        <Tip
+          labelFor="friendTip"
+          label="How much your friend would like to tip?"
+          tipAmount={friendTip}
+          setTipAmount={setFriendTip}
+        >
+          <option value="0">0%</option>
+          <option value="10">10%</option>
+          <option value="15">15%</option>
+          <option value="20">20%</option>
+        </Tip>
+      </form>
+
+      <h2>
+        You Pay ${Number.isNaN(totalTip) ? "0" : totalBill} ($
+        {bill === undefined ? "0" : bill} + $
+        {Number.isNaN(totalTip) ? "0" : totalTip} tip)
+      </h2>
+
+      <ResetButton onClick={reset}>Reset</ResetButton>
     </div>
   );
+}
+
+function Tip({ labelFor, label, children, tipAmount, setTipAmount }) {
+  return (
+    <div>
+      <label for={labelFor}>{label}</label>
+      <select
+        id={labelFor}
+        name={labelFor}
+        value={tipAmount}
+        onChange={(e) => setTipAmount(e.target.value)}
+      >
+        {children}
+      </select>
+    </div>
+  );
+}
+
+function ResetButton({ onClick, children }) {
+  return <button onClick={onClick}>{children}</button>;
 }
 
 export default App;
