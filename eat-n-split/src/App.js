0487ba5380920d40fceea4609329d725006c9a16
Shivam Kumar Pandey <itsshivampandey@gmail.com>
2025-10-09 17:18:59 +0530

    Created SplitBill component

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -1,20 +1,70 @@
+const friends = [
+  {
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=clark",
+    balance: -7,
+  },
+  {
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=sarah",
+    balance: 20,
+  },
+  {
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=anthony",
+    balance: 0,
+  },
+];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Button onClick={() => console.log("clicked")}>Select</Button>
+      <SplitBill />
     </div>
+  );
+}
+
+function Friend() {}
+
+function SplitBill({ name }) {
+  return (
+    <form className="form-split-bill">
+      <Input id="bill" value="" onChange={() => {}}>
+        <span>💰</span> Bill Value
+      </Input>
+      <Input id="your-expense" value="" onChange={() => {}}>
+        <span>🧍‍♂️</span> Your Expense
+      </Input>
+      <Input id="friend-expense" value="" onChange={() => {}}>
+        <span>👯‍♂️</span> {name}'s expense:
+      </Input>
+      <label for="payee">
+        <span>🤑</span>Who is Paying the Bill?
+      </label>
+      <select id="payee">
+        <option value="you">You</option>
+        <option value="friend">{name}</option>
+      </select>
+      <Button onClick={() => {}}>Split Bill</Button>
+    </form>
+  );
+}
+
+function Button({ onClick, children }) {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  );
+}
+
+function Input({ value, onChange, children, id }) {
+  return (
+    <>
+      <label for={id}>{children}</label>
+      <input id={id} value={value} onChange={onChange} />
+    </>
   );
 }
 
