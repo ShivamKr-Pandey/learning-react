7b5312f2018d0093eebdd25181ee21f1a127697f
Shivam Kumar Pandey <itsshivampandey@gmail.com>
2025-10-09 17:58:01 +0530

    Created Friend Component

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -19,13 +19,37 @@ const friends = [
 function App() {
   return (
     <div className="app">
-      <Button onClick={() => console.log("clicked")}>Select</Button>
-      <SplitBill />
+      <Friend />
     </div>
   );
 }
 
-function Friend() {}
+function Friend() {
+  return (
+    <div className="sidebar">
+      <ul>
+        {friends.map((friend) => (
+          <li>
+            <img src={friend.image} alt={friend.name} />
+            <h3>{friend.name}</h3>
+            <Button onClick={() => {}}>Select</Button>
+            {friend.balance < 0 && (
+              <p className="red">
+                You owe {friend.name} Rs {Math.abs(friend.balance)}
+              </p>
+            )}
+            {friend.balance > 0 && (
+              <p className="green">
+                {friend.name} owes you Rs {friend.balance}
+              </p>
+            )}
+            {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 function SplitBill({ name }) {
   return (
